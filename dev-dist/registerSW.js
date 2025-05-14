3325a8934fd9778e13be57a3f5980cba0745ed2c
Luis Waldman <luiswaldman@gmail.com>
2025-05-14 17:34:35 -0300

    agrego configuracion para hacerla progresiva

---
+++ dev-dist/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) navigator.serviceWorker.register('/fogon-cliente/dev-sw.js?dev-sw', { scope: '/fogon-cliente/', type: 'classic' })
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,42 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/fogon-cliente/', // Nombre del repo
-  plugins: [vue()],
+  plugins: [vue(),VitePWA({
+      registerType: 'autoUpdate', // 🔁 auto actualiza si hay una nueva versión
+      devOptions: {
+        enabled: true // ✅ para que funcione durante desarrollo
+      },
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Fogón',
+        short_name: 'Fogón',
+        description: 'App para músicos: letras, acordes y partituras colaborativas',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
