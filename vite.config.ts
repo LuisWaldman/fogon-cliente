19600116eee9e1b7bab6c25337519078235e02b4
Luis Waldman <luiswaldman@gmail.com>
2025-05-16 12:53:31 -0300

    cambio la base en el vite.config para verlo desde www.fogon.ar

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fogon-cliente/', // Nombre del repo
+  base: '/', 
   plugins: [
     vue(),
     VitePWA({
