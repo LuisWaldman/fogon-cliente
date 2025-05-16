6c3d88ec427b23107aa03a819213a395de969b53
Luis Waldman <luiswaldman@gmail.com>
2025-05-16 16:00:34 -0300

    Agrego Icono icono

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div>EL FOGON PROXIMAMENTE</div>
-  <HelloWorld msg="MENSAJE QUE MANDA" />
+  <HelloWorld/>
 </template>
 
 <style scoped>
