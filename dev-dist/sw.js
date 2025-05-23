65e0dd20217d0063b7ed2842397c808ade87767c
Luis Waldman <luiswaldman@gmail.com>
2025-05-23 19:17:35 -0300

    agrego cancion dont wanna miss you blues

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.9mq39qr01u"
+    "revision": "0.uuc8u09i12o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
