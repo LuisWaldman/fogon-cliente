0a52d972c4e5ae2c911741a86d77e2da766a09e9
Luis Waldman <luiswaldman@gmail.com>
2025-05-28 19:18:36 -0300

    conecto app proxima

---
+++ src/AppProxima.vue
@@ -0,0 +1,61 @@
+<script setup lang="ts">
+</script>
+
+<template>
+
+
+<h1>EL FOGON</h1>
+
+<div id="contenedor-musical">
+  <div class="cancion">
+    <h2>Bienvenido al Fogon</h2>
+    <p>Esta es una red musical distribuida.</p>
+    <p>Pronto podrás tocar música con tus amigos.</p>
+  </div>
+</div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+#contenedor-musical {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.cancion {
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin-top: 20px;
+}
+
+.fixed-bottom-right {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  z-index: 9999; /* Asegura que se muestre encima de otros elementos */
+}
+.carteliniciando {
+  position: absolute;
+  top: 20px;
+  font-size: 500px;
+  border: 5px solid #a9a8f6;
+  margin-left: 300px;
+  padding-left: 40px;
+  padding-right: 40px;
+  border-radius: 60px;
+}
+</style>
