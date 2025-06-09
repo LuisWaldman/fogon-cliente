cdba1b2f1e6b1d2d3d7a0ce98f72da92f50182b2
Luis Waldman <luiswaldman@gmail.com>
2025-06-09 00:38:01 -0300

    quito margenes en acordes

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j8pmsr3qac"
+    "revision": "0.bf1seu1mee"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
