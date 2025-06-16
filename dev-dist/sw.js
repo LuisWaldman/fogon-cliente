cc2123484e3d0ac6baab7cda5bd60959c1a38790
Luis Waldman <luiswaldman@gmail.com>
2025-06-16 15:47:04 -0300

    maqueta sesion

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cdah8sap5no"
+    "revision": "0.hb527tt6q4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/comp_configurar/configSesion.vue
+++ src/components/comp_configurar/configSesion.vue
@@ -1,5 +1,60 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAppStore } from '../../stores/appStore'
+import { Sesion } from '../../modelo/sesion'
+import { ref, onMounted } from 'vue'
 
-<template></template>
+const sesiones = ref([] as Sesion[])
+const newsesio = ref(new Sesion('', 0, '', 0, 0))
 
-<style scoped></style>
+function agregarSesion() {
+    sesiones.value.push({ ...newsesio.value })
+    newsesio.value = new Sesion('', 0, '', 0, 0)
+}
+
+</script>
+<template>
+    <div>
+        <form @submit.prevent="agregarSesion">
+            <label for="nombre">Nombre de la sesión:</label>
+            <input id="nombre" v-model="newsesio.nombre" required />
+            <button type="submit">Iniciar Sesión</button>
+        </form>
+
+        <table v-if="sesiones.length">
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Usuarios</th>
+                    <th>Estado</th>
+                    <th>Latitud</th>
+                    <th>Longitud</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(sesion, idx) in sesiones" :key="idx">
+                    <td>{{ sesion.nombre }}</td>
+                    <td>{{ sesion.usuarios }}</td>
+                    <td>{{ sesion.estado }}</td>
+                    <td>{{ sesion.latitud }}</td>
+                    <td>{{ sesion.longitud }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<style scoped>
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 1em;
+}
+th, td {
+    border: 1px solid #ccc;
+    padding: 0.5em;
+    text-align: left;
+}
+form {
+    margin-bottom: 1em;
+}
+</style>
