474387ea6413de61eac49365ad8d78095ac0eac1
Luis Waldman <luiswaldman@gmail.com>
2025-06-16 14:55:56 -0300

    FIX LINT

--- src/components/comp_configurar/configSesion.vue
+++ src/components/comp_configurar/configSesion.vue
@@ -1,11 +1,4 @@
-<script setup lang="ts">
-import { useAppStore } from '../../stores/appStore'
-import { Sesion } from '../../modelo/sesion'
-import { ref, onMounted } from 'vue'
-
-const sesiones = ref([] as Sesion[])
-const newsesio = ref(new Sesion('', 0, '', 0, 0))
-</script>
+<script setup lang="ts"></script>
 
 <template></template>
 
