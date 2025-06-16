d28a188ceee6db7dca3334301043cb6bb95ad73e
Luis Waldman <luiswaldman@gmail.com>
2025-06-16 14:51:53 -0300

    agrego clase sesion

--- src/components/comp_configurar/configSesion.vue
+++ src/components/comp_configurar/configSesion.vue
@@ -1,4 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useAppStore } from '../../stores/appStore'
+import { Sesion } from '../../modelo/sesion'
+import { ref, onMounted } from 'vue'
+
+const sesiones = ref([] as Sesion[])
+const newsesio = ref(new Sesion('', 0, '', 0, 0))
+</script>
+
 <template></template>
 
 <style scoped></style>
