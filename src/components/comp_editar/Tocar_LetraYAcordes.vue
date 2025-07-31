cb05e926f93b09c83dfb53fc6bab5fb9a8957bfd
Luis Waldman <luiswaldman@gmail.com>
2025-07-30 21:50:43 -0300

    arreglo edicion basica desconectado

---
+++ src/components/comp_editar/Tocar_LetraYAcordes.vue
@@ -0,0 +1,246 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { Cancion } from '../../modelo/cancion'
+import { Pantalla } from '../../modelo/pantalla'
+
+const props = defineProps<{
+  compas: number
+  cancion: Cancion
+}>()
+const pantalla = new Pantalla()
+const letraDiv = ref<HTMLElement | null>(null) // Ref to the div
+const mostrandoParte = ref(-1)
+const mostrandoCompasParte = ref(-1)
+const currentCompas = ref(0)
+const letras = ref([] as string[][])
+
+watch(
+  () => props.cancion,
+  (cancion: Cancion) => {
+    actualizarLetras(cancion)
+  },
+)
+
+function actualizarLetras(cancion: Cancion) {
+  let contadorRenglontexto = 0
+  let contadorRenglonParteTexto = 0
+  let nuevaLetra = [] as string[][]
+  for (var i = 0; i < cancion.acordes.ordenPartes.length; i++) {
+    let nuevoRenglon = [] as string[]
+
+    for (
+      var j = 0;
+      j < cancion.acordes.partes[cancion.acordes.ordenPartes[i]].acordes.length;
+      j++
+    ) {
+      nuevoRenglon.push(
+        cancion.letras.renglones[contadorRenglontexto][
+          contadorRenglonParteTexto
+        ],
+      )
+      contadorRenglonParteTexto++
+      if (
+        contadorRenglonParteTexto >=
+        cancion.letras.renglones[contadorRenglontexto].length
+      ) {
+        contadorRenglontexto++
+        contadorRenglonParteTexto = 0
+      }
+    }
+    nuevaLetra.push(nuevoRenglon)
+  }
+  letras.value = nuevaLetra
+}
+
+watch(
+  () => props.compas,
+  (newCompas) => {
+    let totalCompases = 0
+
+    for (let i = 0; i < props.cancion.acordes.ordenPartes.length; i++) {
+      let compasesxparte =
+        props.cancion.acordes.partes[props.cancion.acordes.ordenPartes[i]]
+          .acordes.length
+      if (newCompas < totalCompases + compasesxparte) {
+        mostrandoParte.value = i
+        mostrandoCompasParte.value = newCompas - totalCompases
+        break
+      }
+      totalCompases += compasesxparte
+    }
+    currentCompas.value = newCompas
+
+    const renglon = props.cancion.letras.RenglonDelCompas(newCompas)
+    const configuracionPantalla = pantalla.getConfiguracionPantalla()
+    const tamanioLetra = configuracionPantalla.tamanioLetra
+    const tamanioAcorde = configuracionPantalla.tamanioAcorde
+    const factorScroll = configuracionPantalla.factorScroll // Usar la nueva propiedad
+    let ve = renglon * (tamanioLetra + tamanioAcorde) * factorScroll // Usar la nueva propiedad
+    ve -= (tamanioLetra + tamanioAcorde) * 10
+    const nuevaPos = Math.max(ve, 0)
+
+    moverScroll(nuevaPos)
+  },
+)
+
+function moverScroll(posX: number) {
+  letraDiv.value?.scrollTo({ top: posX, behavior: 'smooth' })
+}
+
+function Actualizar() {
+  if (letras.value.length === 0) {
+    actualizarLetras(props.cancion)
+  }
+  return false
+}
+function styleDivTocar() {
+  return {
+    height: pantalla.getAltoPantalla() + 'px',
+  }
+}
+
+defineExpose({ Actualizar })
+</script>
+<template>
+  <div>
+    <div
+      v-if="letras.length == 0 && cancion.letras.renglones.length > 0"
+      @click="Actualizar"
+    >
+      .. No cargada ..
+    </div>
+    <div
+      style="position: relative"
+      class="componenteMusical"
+      v-if="letras.length > 0 && cancion.letras.renglones.length > 0"
+    >
+      <div
+        ref="letraDiv"
+        class="overflow-auto divDeLetra"
+        :style="styleDivTocar()"
+      >
+        <div style="display: flex; flex-wrap: wrap">
+          <template
+            v-for="(parte, index) in cancion.acordes.ordenPartes"
+            :key="index"
+          >
+            <template
+              v-for="(aco, index_aco) in cancion.acordes.partes[parte].acordes"
+              :key="index_aco"
+            >
+              <!-- SIN ENTER -->
+              <div
+                v-if="!letras[index][index_aco].includes('/n')"
+                :class="{
+                  en_compas:
+                    mostrandoParte === index &&
+                    mostrandoCompasParte === index_aco,
+                }"
+              >
+                <div>
+                  <div class="acordediv">{{ aco }}</div>
+                </div>
+                <div class="divletra">{{ letras[index][index_aco] }}&nbsp;</div>
+              </div>
+
+              <!-- 1 CON ENTER -->
+              <div
+                v-if="
+                  letras[index][index_aco] &&
+                  letras[index][index_aco].includes('/n')
+                "
+                :class="{
+                  en_compas:
+                    mostrandoParte === index &&
+                    mostrandoCompasParte === index_aco,
+                }"
+              >
+                <div>
+                  <div class="acordediv">{{ aco }}</div>
+                </div>
+                <div class="divletra">
+                  {{ letras[index][index_aco].split('/n')[0] }}
+                </div>
+              </div>
+
+              <!-- 1 BREACK CON ENTER-->
+              <div
+                class="break"
+                v-if="
+                  letras[index][index_aco] &&
+                  letras[index][index_aco].includes('/n')
+                "
+              ></div>
+              <!-- 1 SIN ENTER-->
+              <div
+                v-if="
+                  letras[index][index_aco] &&
+                  letras[index][index_aco].includes('/n')
+                "
+                :class="{
+                  en_compas:
+                    mostrandoParte === index &&
+                    mostrandoCompasParte === index_aco,
+                }"
+              >
+                <div>
+                  <div class="acordediv">&nbsp;</div>
+                </div>
+                <div class="divletra">
+                  {{ letras[index][index_aco].split('/n')[1] }}
+                </div>
+              </div>
+            </template>
+          </template>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.divDeLetra {
+  scrollbar-color: black transparent;
+  scrollbar-width: thin;
+}
+
+.componenteMusical {
+  color: white;
+  padding: 6px;
+  height: 100%;
+}
+.read-the-docs {
+  color: #888;
+}
+
+.break {
+  flex-basis: 100%;
+}
+.parte {
+  display: flex;
+}
+.acordediv {
+  font-size: var(--tamanio-acorde);
+  margin: 1px;
+  border-radius: 5px;
+  display: inline-block;
+  color: #a9a8f6;
+  margin-right: 4px;
+}
+
+.ordenparte {
+  border: 1px solid #888;
+  width: 25%;
+}
+.divletra {
+  font-size: var(--tamanio-letra);
+}
+
+.en_compas .acordediv {
+  background-color: red;
+  color: white;
+}
+.en_compas .divletra {
+  color: red;
+}
+</style>
--- src/views/editar.vue
+++ src/views/editar.vue
@@ -3,12 +3,20 @@ import { useAppStore } from '../stores/appStore'
 import cabecera from '../components/comp_editar/editarcabecera.vue'
 import editartexto from '../components/comp_editar/editartexto.vue'
 import editAcordes from '../components/comp_editar/editAcordes.vue'
-
+import TocarLetraAcorde from '../components/comp_editar/Tocar_LetraYAcordes.vue'
 import { ref, type Ref } from 'vue'
 import { Pantalla } from '../modelo/pantalla'
+import { useRouter } from 'vue-router'
 
 const pantalla = new Pantalla()
 
+class vistaTocar {
+  viendo: string = 'acordes'
+  secuencia: boolean = true
+  partes: boolean = true
+  proximosAcordes: boolean = false
+}
+const vista: Ref<vistaTocar> = ref(new vistaTocar())
 function GetStylePantallaEdit() {
   return {
     width: pantalla.getAnchoPantalla() + 'px',
@@ -31,29 +39,101 @@ function claseVistaSecundaria() {
 const appStore = useAppStore()
 const ctrlEditarTexto = ref()
 
+function cambiarVista(nvista: string) {
+  vista.value.viendo = nvista
+  localStorage.setItem('viendo_vista_editando', nvista)
+}
 ctrlEditarTexto.value?.updateContent()
+
+const router = useRouter()
+function clickTocar() {
+  // Redirect to edit page for the current song
+  appStore.cancion = appStore.editandocancion
+  router.push('/tocar')
+}
+
 </script>
 <template>
   <cabecera></cabecera>
   <div style="display: flex"
+   class="relativo"
   :style="GetStylePantallaEdit()">
     <div style="width: 70%;" :style="claseVistaPrincipal()">
+      <TocarLetraAcorde
+          v-if="vista.viendo == 'acordes'"
+          :cancion="appStore.cancion"
+          :compas="appStore.compas"
+        ></TocarLetraAcorde>
+
       <editartexto
+        v-if="vista.viendo == 'editartexto'"
         :ref="ctrlEditarTexto"
         :cancion="appStore.editandocancion"
         :compas="appStore.compas"
       ></editartexto>
     </div>
+
     <div :style="claseVistaSecundaria()">
+
       <editAcordes
         :cancion="appStore.editandocancion"
         :compas="appStore.compas"
       ></editAcordes>
     </div>
+    
+      <div class="dropdown dropdown-superior-derecha">
+        <button
+          class="btn btn-secondary dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          <i class="bi bi-eye"></i>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <li v-on:click="cambiarVista('acordes')">
+            <a class="dropdown-item" href="#">Acordes</a>
+          </li>
+          <li v-on:click="cambiarVista('soloacordes')">
+            <a class="dropdown-item" href="#">Editar Texto Acordes</a>
+          </li>
+          <li v-on:click="cambiarVista('solometrica')">
+            <a class="dropdown-item" href="#">Editar Metrica</a>
+          </li>
+          <li><hr class="dropdown-divider" /></li>
+
+          <li>
+            <a class="dropdown-item" href="#">
+              <i class="bi bi-check-circle" v-if="vista.secuencia"></i>
+              Editar Acordes</a
+            >
+          </li>
+          <li><hr class="dropdown-divider" /></li>
+          <li >
+            <a class="dropdown-item" href="#"> Ajustar Tamaños</a>
+          </li>
+
+          <li><hr class="dropdown-divider" /></li>
+          <li @click="clickTocar()">
+            <a class="dropdown-item" href="#">Tocar</a>
+          </li>
+        </ul>
+      </div>
   </div>
 </template>
 <style scoped>
 .read-the-docs {
   color: #888;
+}
+
+.relativo {
+  position: relative;
+}
+.dropdown-superior-derecha {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  z-index: 10;
 }
 </style>
