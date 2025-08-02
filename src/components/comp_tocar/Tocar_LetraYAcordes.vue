101b89f82f0fb188659bd9aa84f1e55eb1cf4672
Luis Waldman <luiswaldman@gmail.com>
2025-08-02 15:56:45 -0300

    fix cambio de pantalla en letra y acordes

--- src/components/comp_tocar/Tocar_LetraYAcordes.vue
+++ src/components/comp_tocar/Tocar_LetraYAcordes.vue
@@ -5,6 +5,7 @@ import { Pantalla } from '../../modelo/pantalla'
 
 const props = defineProps<{
   compas: number
+  nroVista: number
   cancion: Cancion
 }>()
 const pantalla = new Pantalla()
@@ -21,9 +22,9 @@ watch(
   },
 )
 watch(
-  () => props.compas,
+  () => props.nroVista,
   () => {
-    actualizarLetras(props.cancion)
+    Actualizar()
   },
 )
 
@@ -61,6 +62,7 @@ function actualizarLetras(cancion: Cancion) {
 watch(
   () => props.compas,
   (newCompas) => {
+    Actualizar()
     let totalCompases = 0
 
     for (let i = 0; i < props.cancion.acordes.ordenPartes.length; i++) {
@@ -105,12 +107,20 @@ function styleDivTocar() {
   }
 }
 
+function Actualizado() {
+  if (props.cancion.letras.renglones.length > 0 && letras.value.length === 0) {
+    actualizarLetras(props.cancion)
+  }
+  return false
+
+}
+
 defineExpose({ Actualizar })
 </script>
 <template>
   <div>
     <div
-      v-if="letras.length == 0 && cancion.letras.renglones.length > 0"
+      v-if="Actualizado()"
       @click="Actualizar"
     >
       .. No cargada ..
