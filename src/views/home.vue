1fea5ea9045bc61ffb69b969a7370b5dfcd319bb
Luis Waldman <luiswaldman@gmail.com>
2025-08-04 19:56:49 -0300

    fix lint

--- src/views/home.vue
+++ src/views/home.vue
@@ -10,7 +10,9 @@ const appStore = useAppStore()
     <h1 style="color: blueviolet; margin-bottom: 0px; padding-bottom: 0px">
       Bienvenido al Fogon
     </h1>
-    <span class="version">V. SINCRONIZADA (perfiles de usuario para ¿tiempo real?)</span>
+    <span class="version"
+      >V. SINCRONIZADA (perfiles de usuario para ¿tiempo real?)</span
+    >
 
     <p class="primer-parrafo" v-if="appStore.estado === 'conectando'">
       Esta desconectado! No Pasa nada, el fogon esta preparado para funcionar
