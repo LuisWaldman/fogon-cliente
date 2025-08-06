30dd76a48fe83d27f3499b5229b31029d62e0528
Luis Waldman <luiswaldman@gmail.com>
2025-08-05 23:57:03 -0300

    nueva version

--- src/views/home.vue
+++ src/views/home.vue
@@ -11,7 +11,7 @@ const appStore = useAppStore()
       Bienvenido al Fogon
     </h1>
     <span class="version"
-      >V. SINCRONIZADA (nuevo reloj)</span
+      >V. SINCRONIZADA (arma fogon)</span
     >
 
     <p class="primer-parrafo" v-if="appStore.estado === 'conectando'">
