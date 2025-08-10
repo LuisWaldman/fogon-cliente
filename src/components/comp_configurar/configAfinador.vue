ee7e42a43cddbc971ed2aae624aeca3536bf2d73
Luis Waldman <luiswaldman@gmail.com>
2025-08-10 13:51:50 -0300

    Agrego control afinador

---
+++ src/components/comp_configurar/configAfinador.vue
@@ -0,0 +1,26 @@
+
+<script setup lang="ts">
+import { Pantalla } from '../../modelo/pantalla';
+
+const pantalla = new Pantalla()
+const ancho = pantalla.getAnchoPantalla() * 0.7
+const alto = pantalla.getAltoPantalla()
+function styleDivAfinador() {
+  return {
+    height: alto + 'px',
+    width: ancho + 'px',
+  }
+}
+</script>
+
+<template>
+  <div :style="styleDivAfinador()" class="divAfinador">
+      Afinador
+  </div>
+</template>
+
+<style scoped>
+.divAfinador {
+  border: 1px solid blue;
+}
+</style>
