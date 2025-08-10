a6f54d3c428f9742746af25a34b431b0205052b0
Luis Waldman <luiswaldman@gmail.com>
2025-08-10 18:00:58 -0300

    muestra las lines de afinacion

--- src/components/comp_configurar/configAfinador.vue
+++ src/components/comp_configurar/configAfinador.vue
@@ -7,57 +7,30 @@ const ancho = pantalla.getAnchoPantalla() * 0.7
 const alto = pantalla.getAltoPantalla()
 const tipoAfinacion = ref(440) // 440 Hz por defecto
 const cantidadNotas = ref(12) // Cantidad de notas en la afinación
-const muestraNotas = ref(14) // Cantidad de notas en la afinación
-const muestraNotasAnteriores = ref(5) // Cantidad de notas en la afinación
 const Notas = ref<number[]>([]) // Cantidad de notas en la afinación
 const ClaseNotas = ref<string[]>([])
 const NotasAnteriores = ref<number[]>([]) // Cantidad de notas en la afinación
 const ClaseNotasAnteriores = ref<string[]>([])
-
+const octavasCirculo = ref(8)
+const DesdeOctavasCirculo = ref(3)
+const refViendoFrecuencia = ref(440)
+function clickVerFrecuencia(frecuencia: number) {
+  refViendoFrecuencia.value = frecuencia
+}
 function calcularNotas() {
   Notas.value = []
-  ClaseNotas.value = []
   NotasAnteriores.value = []
-  ClaseNotasAnteriores.value = []
-  
-  // Calcular notas anteriores (frecuencias menores)
-  for (let i = 1; i < muestraNotasAnteriores.value; i++) {
-    const afina = tipoAfinacion.value * Math.pow(2, -i / cantidadNotas.value)
-    NotasAnteriores.value.unshift(afina) // unshift para mantener el orden ascendente
-    const ratio = tipoAfinacion.value / afina
-    
-    // Verificar si es octava (múltiplos de 2)
-    if (Math.abs(ratio - Math.round(ratio)) < 0.001 && Math.round(ratio) > 1) {
-      ClaseNotasAnteriores.value.unshift('octava')
-    } 
-    // Verificar si es quinta (séptimo semitono en 12-TET)
-    else if (i % cantidadNotas.value === 7) {
-      ClaseNotasAnteriores.value.unshift('quinta')
-    } 
-    else {
-      ClaseNotasAnteriores.value.unshift(`Nota -${i}`)
-    }
+  const desdeNota = tipoAfinacion.value / 4
+// cantidadNotas es la cantidad de notas en la octava
+  for (let i = 0; i < cantidadNotas.value * 2; i++) {
+    const nota = desdeNota * Math.pow(2, i / cantidadNotas.value)
+    NotasAnteriores.value.push(nota)
   }
-  
-  // Calcular notas posteriores (frecuencias mayores) - código existente
-  for (let i = 1; i < muestraNotas.value; i++) {
-    const afina = tipoAfinacion.value * Math.pow(2, i / cantidadNotas.value)
-    Notas.value.push(afina)
-    const ratio = afina / tipoAfinacion.value
-    
-    // Verificar si es octava (múltiplos de 2)
-    if (Math.abs(ratio - Math.round(ratio)) < 0.001 && Math.round(ratio) > 1) {
-      ClaseNotas.value.push('octava')
-    } 
-    // Verificar si es quinta (séptimo semitono en 12-TET)
-    else if (i % cantidadNotas.value === 7) {
-      ClaseNotas.value.push('quinta')
-    } 
-    else {
-      ClaseNotas.value.push(`Nota ${i}`)
-    }
+  for (let i = 1; i < cantidadNotas.value * 3; i++) {
+    const nota = tipoAfinacion.value * Math.pow(2, i / cantidadNotas.value)
+    Notas.value.push(nota)
   }
-}
+  }
 
 calcularNotas()
 
@@ -67,37 +40,143 @@ function styleDivAfinador() {
     width: ancho + 'px',
   }
 }
+
+  const maxRadio = 500
+  const minRadio = 50
+  const centroLeft = 300
+  const centroTop = 230
+function StyleOctava(i: number) {
+  const radio = minRadio + ((maxRadio - minRadio) / (octavasCirculo.value - 1)) * (i - 1)
+  const left = centroLeft - radio / 2
+  const top = centroTop - radio / 2
+  return {
+    width: radio + 'px',
+    top: top + 'px',
+    left: left + 'px',
+    height: radio + 'px',
+    borderRadius: '50%',
+  }
+}
+function StyleFrecuencia(frecuencia: number) {
+  let enOctava = Math.floor(Math.log2(frecuencia / tipoAfinacion.value)) + DesdeOctavasCirculo.value
+  const baseOctava = tipoAfinacion.value * Math.pow(2, Math.floor(Math.log2(frecuencia / tipoAfinacion.value)))
+  const portentajeEnOctava = (frecuencia - baseOctava) / tipoAfinacion.value 
+
+  if (enOctava < 0) {
+    enOctava = 0
+  }
+  // Calcular el porcentaje de la octava
+  const radio = minRadio + ((maxRadio - minRadio) / (octavasCirculo.value - 1)) * (enOctava - 1)
+  const left = centroLeft + Math.cos(portentajeEnOctava * 2 * Math.PI) * (radio / 2)
+  const top = centroTop + Math.sin(portentajeEnOctava * 2 * Math.PI) * (radio / 2)
+  console.log('enOctava', enOctava,baseOctava, "Porcentaje=",portentajeEnOctava, frecuencia, tipoAfinacion.value)
+  return {
+    top: top + 'px',
+    left: left + 'px',
+  }
+}
+
+function StyleFrecuenciaLinea(frecuencia: number) {
+  let fontSize = 10
+  let backgroundColor = ''
+  let border = ''
+  let color = 'white'
+  
+  // Calcular en qué octava está la frecuencia relativa a tipoAfinacion
+  const octavasDesdeBase = Math.log2(frecuencia / tipoAfinacion.value)
+  const octavaCompleta = Math.floor(octavasDesdeBase)
+  const baseOctava = tipoAfinacion.value * Math.pow(2, octavaCompleta)
+  const portentajeEnOctava = (frecuencia - baseOctava) / baseOctava
+
+  // Determine styling based on the remainder
+  if (Math.abs(portentajeEnOctava - 0) < 0.01 || Math.abs(portentajeEnOctava - 1) < 0.01) {
+    fontSize = 24
+    color = 'red'
+    border = '1px solid black'
+  } else if (Math.abs(portentajeEnOctava - 0.5) < 0.02) {
+    fontSize = 22
+    border = '1px solid rgb(102, 64, 64);'
+  } else if (Math.abs(portentajeEnOctava - (1/3)) < 0.02) {
+    fontSize = 19
+    border = '1px solid rgb(102, 64, 64);'
+  }
+  return {
+    'font-size': fontSize + 'px',
+    'background-color': backgroundColor,
+    color: color,
+    border: border,
+    'padding-top': '-12px',
+  }
+}
+
+
 </script>
 
 <template>
   <div :style="styleDivAfinador()" class="divAfinador">
       <div style="display: flex;">
-                <div v-for="(nota, index) in NotasAnteriores" :key="index" :class="ClaseNotasAnteriores[index]" class="clsNota">
-          {{ nota.toFixed(2) }} 
+                <div v-for="(nota, index) in NotasAnteriores" @click="clickVerFrecuencia(nota)" :key="index" :style="StyleFrecuenciaLinea(nota)" :class="ClaseNotasAnteriores[index]" class="clsNota">
+          {{ nota.toFixed(0) }} 
         </div>
-        <input type="number" v-model="tipoAfinacion" min="1" max="999" @change="calcularNotas" />
-        <div v-for="(nota, index) in Notas" :key="index" :class="ClaseNotas[index]" class="clsNota">
-          {{ nota.toFixed(2) }} 
+        <input type="number" v-model="tipoAfinacion" min="1" max="999" @focus="clickVerFrecuencia(tipoAfinacion)" @change="calcularNotas" />
+        <div v-for="(nota, index) in Notas" @click="clickVerFrecuencia(nota)" :key="index" :class="ClaseNotas[index]" :style="StyleFrecuenciaLinea(nota)"  class="clsNota">
+          {{ nota.toFixed(0) }} 
         </div>
+      </div>
+      <div class="circulodiv">
+        <div v-for="i in octavasCirculo" :key="i" ><div :style="StyleOctava(i)" class="circuloOctava"></div></div>
+
+
+        <div :style="StyleFrecuencia(Number(refViendoFrecuencia.toFixed(0)))" class="frecuencia viendoFrecuencia">
+          {{ refViendoFrecuencia.toFixed(0) }}
+        </div>
+        <div v-for="k in 3" :key="k" :style="StyleFrecuencia(tipoAfinacion * Math.pow(2, (k-1) * 2))" class="frecuencia">
+         {{ (tipoAfinacion * Math.pow(2, (k-1) * 2)).toFixed(0) }}
+        </div>
+        <div>
+        <div  v-for="k in 3" :key="k" :style="StyleFrecuencia(440 * Math.pow(2, (k-1) * 2))" class="frecuencia">
+          {{ (440 * Math.pow(2, (k-1) * 2)).toFixed(0) }}
+        </div>
+        
+      </div>
       </div>
 
   </div>
 </template>
 
 <style scoped>
+.circulodiv {
+  position: relative;
+}
+.circuloOctava {
+  position: absolute;
+  border-radius: 50%;
+  border: 1px solid rgb(133, 104, 202);
+}
+.viendoFrecuencia {
+  background-color:  rgb(133, 104, 202);
+  color: white;
+  padding: 2px;
+  border: 1px solid black;
+}
+.frecuencia {
+  position: absolute;
+  
+}
 .divAfinador {
-  border: 1px solid blue;
+  
 }
 
 .quinta {
   font-size: larger;
+  background-color: lightyellow;
 }
 .octava {
+  background-color: lightgrey;
   font-size: x-large;
 }
 .clsNota {
-  margin-right: 5px;
-  padding: 2px;
-  border: 1px solid;
+  padding: 1px;
+  height: 30px;
 }
 </style>
