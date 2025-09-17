52d32ba0bd2806526d8c4173804be13cd5ae745a
Luis Waldman <luiswaldman@gmail.com>
2025-09-17 16:46:19 -0300

    cambio icono

--- src/components/comp_cabecera/iconofogon.vue
+++ src/components/comp_cabecera/iconofogon.vue
@@ -141,7 +141,7 @@ function clickFogon() {
 
       <div style="margin-top: 50px">
         <div class="titulo-Fogon" v-if="appStore.estadoSesion != 'conectado'">
-          El Fogon RTC
+          El Fogon toca
         </div>
         <div class="titulo-Fogon" v-if="appStore.estadoSesion === 'conectado'">
           {{ appStore.sesion.nombre }}
