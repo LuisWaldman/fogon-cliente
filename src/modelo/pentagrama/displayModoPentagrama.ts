823f350e2b1b04bd9e0731a4a8eba99b94035287
Luis Waldman <luiswaldman@gmail.com>
2025-09-29 22:50:00 -0300

    Fix lint

--- src/modelo/pentagrama/displayModoPentagrama.ts
+++ src/modelo/pentagrama/displayModoPentagrama.ts
@@ -1,7 +1,7 @@
 export class DisplayModoPentagrama {
   public Ver: boolean
   public Nombre: string
-  public Instrumento: string = ""
+  public Instrumento: string = ''
   public Claves: string[] = []
   public constructor(Nombre: string, Ver: boolean) {
     this.Ver = Ver
