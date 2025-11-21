7307b5dcecbb50aedd1222f66d2aa98dd88d7bd6
Luis Waldman <luiswaldman@gmail.com>
2025-11-21 19:41:01 -0300

    mejoro sombra

--- src/components/comp_editar/editarcabecera.vue
+++ src/components/comp_editar/editarcabecera.vue
@@ -529,8 +529,8 @@ function DescargarJSON() {
 }
 
 .edintandoCtrl {
-  border: 2px solid #cfda41;
-  background: linear-gradient(135deg, #000000 0%, #cfda41 100%);
+  border: 2px solid #92974e;
+  background: linear-gradient(55deg, #000000 50%, #cfda41 100%);
 }
 .titulocancion {
   width: 400px;
