8556ac4dfd7962f7b8555b76488581b4486311db
Luis Waldman <luiswaldman@gmail.com>
2025-11-23 16:54:02 -0300

    dejo un solo renglon por verso en la descripcion

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jn4bst8m0h"
+    "revision": "0.an2i165en6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
