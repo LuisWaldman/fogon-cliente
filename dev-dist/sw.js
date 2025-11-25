f42ea5d2f7aba9176f5be92a164a89b8c35b75a3
Luis Waldman <luiswaldman@gmail.com>
2025-11-25 11:25:48 -0300

    Vuelvo al color rojo

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.15k36f510qo"
+    "revision": "0.t1jb4bito6"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
