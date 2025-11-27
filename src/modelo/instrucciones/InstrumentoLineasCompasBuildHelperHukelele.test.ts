25299ad52b17e186b21b9fb1da8940f0f8653023
Luis Waldman <luiswaldman@gmail.com>
2025-11-26 21:00:57 -0300

    Fix test

--- src/modelo/instrucciones/InstrumentoLineasCompasBuildHelperHukelele.test.ts
+++ src/modelo/instrucciones/InstrumentoLineasCompasBuildHelperHukelele.test.ts
@@ -1,3 +1,4 @@
+import { describe, expect, it } from 'vitest'
 import { InstrumentoLineasCompasBuildHelperUkelele } from './InstrumentoLineasCompasBuildHelperHukelele'
 
 describe('InstrumentoLineasCompasBuildHelperUkelele', () => {
