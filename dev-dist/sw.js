1557a29de826f9ffc96695966d2c10779d7d663a
Luis Waldman <luiswaldman@gmail.com>
2026-03-02 20:08:48 -0300

    cambios en Lint

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lj2pq4ju9eg"
+    "revision": "0.cc5voloriag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
