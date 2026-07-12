519a667025d109016dea028db0013f8b792e7628
Luis Waldman <luiswaldman@gmail.com>
2026-07-12 16:02:57 -0300

    lint fix

--- src/helpers/WakeLockManager.ts
+++ src/helpers/WakeLockManager.ts
@@ -1,29 +1,44 @@
-// src/helpers/WakeLockManager.ts
+﻿// src/helpers/WakeLockManager.ts
+
+interface WakeLockSentinel extends EventTarget {
+  release(): Promise<void>
+}
+
+interface Navigator {
+  wakeLock?: {
+    request(type: 'screen'): Promise<WakeLockSentinel>
+  }
+}
 
 export class WakeLockManager {
-  private wakeLock: WakeLockSentinel | null = null;
+  private wakeLock: WakeLockSentinel | null = null
 
   public async acquire(): Promise<void> {
     if (!('wakeLock' in navigator)) {
-      console.log('Wake Lock API not supported.');
-      return;
+      console.log('Wake Lock API not supported.')
+      return
     }
 
     try {
-      this.wakeLock = await (navigator as any).wakeLock.request('screen');
-      this.wakeLock.addEventListener('release', () => {
-        console.log('Wake Lock was released');
-      });
-      console.log('Wake Lock is active');
-    } catch (err: any) {
-      console.error(`${err.name}, ${err.message}`);
+      const nav = navigator as Navigator
+      if (nav.wakeLock) {
+        this.wakeLock = await nav.wakeLock.request('screen')
+        this.wakeLock.addEventListener('release', () => {
+          console.log('Wake Lock was released')
+        })
+        console.log('Wake Lock is active')
+      }
+    } catch (err: unknown) {
+      if (err instanceof Error) {
+        console.error(`${err.name}, ${err.message}`)
+      }
     }
   }
 
   public async release(): Promise<void> {
     if (this.wakeLock) {
-      await this.wakeLock.release();
-      this.wakeLock = null;
+      await this.wakeLock.release()
+      this.wakeLock = null
     }
   }
 }
